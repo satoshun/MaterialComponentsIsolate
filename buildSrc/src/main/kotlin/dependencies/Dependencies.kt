8dff842b2030f1efd9adab4c023b5dc669329b01
SatoShun <shun.sato1@gmail.com>
2020-03-12 05:34:19 +0000

    add first

---
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -0,0 +1,34 @@
+package dependencies
+
+const val COMPILE_SDK = 29
+const val MIN_SDK = 14
+const val TARGET_SDK = 29
+
+const val ANDROID_PLUGIN = "com.android.tools.build:gradle:3.6.1"
+const val PUBLISH_PLUGIN = "com.vanniktech:gradle-maven-publish-plugin:0.9.0"
+
+const val KTX = "androidx.core:core-ktx:1.1.0"
+
+const val APPCOMPAT = "androidx.appcompat:appcompat:1.1.0"
+const val RECYCLER_VIEW = "androidx.recyclerview:recyclerview:1.0.0"
+
+const val ACTIVITYX = "androidx.activity:activity-ktx:1.0.0"
+const val FRAGMENTX = "androidx.fragment:fragment-ktx:1.1.0"
+
+const val MATERIAL = "com.google.android.material:material:1.1.0"
+
+const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
+const val LIVEDATA = "androidx.lifecycle:lifecycle-livedata:2.0.0"
+const val LIFECYCLE = "androidx.lifecycle:lifecycle-common:2.2.0-rc01"
+const val LIFECYCLE_RUNTIME = "androidx.lifecycle:lifecycle-runtime-ktx:2.2.0-rc01"
+const val LIFECYCLE_COMPILER = "androidx.lifecycle:lifecycle-compiler:2.0.0"
+const val LIFECYCLE_EXTENSIONS = "androidx.lifecycle:lifecycle-extensions:2.0.0"
+
+const val JUNIT = "junit:junit:4.12"
+const val TRUTH = "com.google.truth:truth:1.0"
+const val TEST_RULES = "androidx.test:rules:1.1.1"
+const val TEST_RUNNER = "androidx.test:runner:1.1.1"
+const val TEST_CORE = "androidx.test:core-ktx:1.1.0"
+const val TEST_JUNIT_RULES = "androidx.test.ext:junit-ktx:1.1.0"
+const val ESPRESSO = "androidx.test.espresso:espresso-core:3.1.1"
+const val ROBOLECTRIC = "org.robolectric:robolectric:4.1"
